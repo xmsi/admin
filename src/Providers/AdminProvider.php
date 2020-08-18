811d90c935f5461581a1bed77ff34e67ba7cd1ed
xmsi <muhammadumarsotvoldiev@gmail.com>
2020-08-18 19:47:01 +0500

    first

---
+++ src/Providers/AdminProvider.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Xmsi\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * 
+ */
+class AdminProvider extends ServiceProvider
+{
+	public function boot()
+	{
+		dd("dasdadsa");
+	}
+
+	public function register()
+	{
+		
+	}
+}
+
+ ?>
